489a9d343a286693ae9420676c9d38366a1bb486
Jon <jon.p.hammer@gmail.com>
2023-04-11 15:07:47 -0400

    Update and register models

--- homedb/inventory/admin.py
+++ homedb/inventory/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Area, Device, DeviceType, House, Room, LightConfiguration
+
+admin.site.register(Area)
+admin.site.register(Device)
+admin.site.register(DeviceType)
+admin.site.register(House)
+admin.site.register(Room)
+admin.site.register(LightConfiguration)
--- homedb/inventory/models.py
+++ homedb/inventory/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class House(models.Model):
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
@@ -40,13 +39,42 @@ class Device(models.Model):
         ('ip', 'IP'),
         ('other', 'Other'),
     ]
+    DEVICE_STATUS_CHOICES = [
+        ('commissioned', 'Commissioned'),
+        ('installed', 'Installed'),
+        ('storage', 'Storage'),
+    ]
 
     name = models.CharField(max_length=255)
     device_type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
-    protocol = models.CharField(max_length=10, choices=DEVICE_PROTOCOL_CHOICES, blank=True, default='')
+    protocol = models.CharField(max_length=32, choices=DEVICE_PROTOCOL_CHOICES, blank=True, default='')
     manufacturer = models.CharField(max_length=255, blank=True, default='')
     model = models.CharField(max_length=255, blank=True, default='')
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    management_url = models.URLField(blank=True, default='')
+    home_assistant_url = models.URLField(blank=True, default='')
+    status = models.CharField(max_length=32, choices=DEVICE_STATUS_CHOICES)
+    mac_address = models.CharField(max_length=255, blank=True, default='')
 
     def __str__(self):
         return self.name
+
+
+class LightConfiguration(models.Model):
+    area = models.ForeignKey(Area, on_delete=models.CASCADE, null=True, blank=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True, blank=True)
+    timer = models.IntegerField()  # In minutes
+    sun_elevation_cutoff = models.FloatField()
+    time_of_day_cutoff = models.TimeField()
+    lux_cutoff = models.FloatField()
+    brightness = models.FloatField()
+
+    def __str__(self):
+        if self.area and self.room:
+            return f"Light Configuration for Area: {self.area.name}, Room: {self.room.name}"
+        elif self.area:
+            return f"Light Configuration for Area: {self.area.name}"
+        elif self.room:
+            return f"Light Configuration for Room: {self.room.name}"
+        else:
+            return f"Unassociated Light Configuration (ID: {self.pk})"
