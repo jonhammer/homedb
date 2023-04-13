3473bc32c9735d98a2e9f36a13bfa7cd151328df
Jon <jon.p.hammer@gmail.com>
2023-04-13 14:33:02 -0400

    Update models

--- homedb/inventory/admin.py
+++ homedb/inventory/admin.py
@@ -1,10 +1,96 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.urls import reverse
+from django.utils.html import format_html
 
-from .models import Area, Device, DeviceType, House, LightConfiguration, Room
+from .models import (
+    Area,
+    BlockOffEntity,
+    BrightnessConfig,
+    Device,
+    DeviceType,
+    House,
+    LightConfig,
+    Room,
+)
 
-admin.site.register(Area)
+
+class BlockOffEntityInline(admin.TabularInline):
+    model = BlockOffEntity
+    extra = 0
+
+
+class BrightnessConfigInline(admin.TabularInline):
+    model = BrightnessConfig
+    extra = 0
+
+
+class LightConfigurationInline(admin.StackedInline):
+    model = LightConfig
+    extra = 0
+
+
+class AreaInline(admin.StackedInline):
+    model = Area
+    extra = 0
+
+
+class HouseAdmin(admin.ModelAdmin):
+    inlines = [AreaInline]
+
+
+class RoomInline(admin.StackedInline):
+    model = Room
+    extra = 0
+
+
+class AreaAdmin(admin.ModelAdmin):
+    inlines = [RoomInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related("house")
+
+
+class RoomAdmin(admin.ModelAdmin):
+    readonly_fields = ("light_configuration_link",)
+
+    def light_configuration_link(self, obj):
+        if obj.lightconfig:
+            url = reverse(
+                "admin:inventory_lightconfig_change",
+                args=[obj.lightconfig.id],
+            )
+            return format_html('<a href="{}">Edit Light Configuration</a>', url)
+        return "No Light Configuration"
+
+    light_configuration_link.short_description = "Light Configuration"
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related("lightconfig", "area")
+
+
+class LightConfigurationAdmin(admin.ModelAdmin):
+    inlines = [BlockOffEntityInline, BrightnessConfigInline]
+
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.clean()
+            super().save_model(request, obj, form, change)
+        except ValidationError as e:
+            form.add_error(None, e)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related("room", "area")
+
+
+admin.site.register(House, HouseAdmin)
 admin.site.register(Device)
 admin.site.register(DeviceType)
-admin.site.register(House)
-admin.site.register(Room)
-admin.site.register(LightConfiguration)
+admin.site.register(Area, AreaAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(LightConfig, LightConfigurationAdmin)
+admin.site.register(BlockOffEntity)
+admin.site.register(BrightnessConfig)
--- homedb/inventory/models.py
+++ homedb/inventory/models.py
@@ -1,7 +1,13 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class House(models.Model):
+    """
+    Represents a house with a name.
+    """
+
     name = models.CharField(max_length=255)
 
     def __str__(self):
@@ -9,6 +15,10 @@ class House(models.Model):
 
 
 class Area(models.Model):
+    """
+    Represents an area within a house, with a name and a reference to the house it belongs to.
+    """
+
     name = models.CharField(max_length=255)
     house = models.ForeignKey(House, on_delete=models.CASCADE)
 
@@ -17,6 +27,10 @@ class Area(models.Model):
 
 
 class Room(models.Model):
+    """
+    Represents a room within an area, with a name and a reference to the area it belongs to.
+    """
+
     name = models.CharField(max_length=255)
     area = models.ForeignKey(Area, on_delete=models.CASCADE)
 
@@ -25,6 +39,10 @@ class Room(models.Model):
 
 
 class DeviceType(models.Model):
+    """
+    Represents a type of device, with a name.
+    """
+
     name = models.CharField(max_length=255)
 
     def __str__(self):
@@ -32,6 +50,11 @@ class DeviceType(models.Model):
 
 
 class Device(models.Model):
+    """
+    Represents a device in a room, with a name, device type, protocol, manufacturer, model, room,
+    management URL, home assistant URL, status, and MAC address.
+    """
+
     DEVICE_PROTOCOL_CHOICES = [
         ("", "Not specified"),
         ("zigbee", "Zigbee"),
@@ -62,29 +85,97 @@ class Device(models.Model):
         return self.name
 
 
-class LightConfiguration(models.Model):
+class LightConfig(models.Model):
+    """
+    Represents a light configuration for either an area or a room, with timer, sun elevation,
+    time cutoffs, and lux cutoff. The configuration must be associated with either an area or
+    a room, but not both.
+    """
+
     area = models.OneToOneField(Area, on_delete=models.CASCADE, null=True, blank=True)
     room = models.OneToOneField(Room, on_delete=models.CASCADE, null=True, blank=True)
-    timer = models.IntegerField()  # In minutes
-    sun_elevation_cutoff = models.FloatField()
-    before_time_cutoff = models.TimeField()
-    after_time_cutoff = models.TimeField()
-    lux_cutoff = models.FloatField()
-    brightness = models.FloatField()
+    timer = models.IntegerField(null=True, blank=True)  # In minutes
+    sun_elevation_below_cutoff = models.IntegerField(blank=True, null=True)
+    sun_elevation_above_cutoff = models.IntegerField(blank=True, null=True)
+    before_time_cutoff = models.TimeField(blank=True, null=True)
+    after_time_cutoff = models.TimeField(blank=True, null=True)
+    lux_cutoff = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         if self.area:
-            return f"Light Configuration for Area: {self.area.name}"
+            return f"{self.area.name}"
         elif self.room:
-            return f"Light Configuration for Room: {self.room.name}"
+            return f"{self.room.name}"
         else:
-            return f"Unassociated Light Configuration (ID: {self.pk})"
+            return "Unnamed Light Configuration"
+
+    def clean(self):
+        if self.area and self.room:
+            raise ValidationError(
+                "Light Configuration cannot be associated with both an Area and a Room."
+            )
+        if not self.area and not self.room:
+            raise ValidationError(
+                "Light Configuration must be associated with either an Area or a Room."
+            )
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
 
-class LightBlockingDevice(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    device = models.ForeignKey(Device, on_delete=models.CASCADE)
-    state = models.CharField(max_length=255)
+class BlockOffEntity(models.Model):
+    """
+    Represents a block off entity associated with a light configuration. It has an entity name
+    and a block state, which by default is set to "on".
+    """
+
+    light_config = models.ForeignKey(LightConfig, on_delete=models.CASCADE)
+    entity_name = models.CharField(max_length=255)
+    block_state = models.CharField(max_length=255, blank=True, default="on")
 
     def __str__(self):
-        return f"Light Blocking Device: {self.device.name} in Room: {self.room.name}"
+        return f"{self.light_config}: {self.entity_name}"
+
+    class Meta:
+        verbose_name_plural = "Block Off Entities"
+
+
+class BrightnessConfig(models.Model):
+    """
+    Represents a brightness configuration associated with a light configuration. It has a
+    brightness value (0-255), and a start time to apply the configuration.
+    """
+
+    light_config = models.ForeignKey(LightConfig, on_delete=models.CASCADE)
+    brightness = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(255)]
+    )
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.light_config}: {self.brightness} at {self.start_time}"
+
+    def save(self, *args, **kwargs):
+        # Ensure start time is before end time
+        if self.start_time >= self.end_time:
+            raise ValidationError("Start time must be before end time.")
+
+        # Check for overlapping time periods
+        overlapping_configs = BrightnessConfig.objects.filter(
+            light_config=self.light_config,
+            start_time__lt=self.end_time,
+            end_time__gt=self.start_time,
+        )
+
+        # Exclude the current instance from the queryset if it's being updated
+        if self.pk:
+            overlapping_configs = overlapping_configs.exclude(pk=self.pk)
+
+        if overlapping_configs.exists():
+            raise ValidationError(
+                "Overlapping time period with an existing BrightnessConfig for the same light_config."
+            )
+
+        super().save(*args, **kwargs)
